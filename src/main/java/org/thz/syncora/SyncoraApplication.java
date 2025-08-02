f0f86ff1e4431ac9a6d240fb216f21d22bdda1a1
Thet Htoo Zaw <thethtoozaw@blue-stone.net>
2025-08-02 22:38:41 +0630

    [INIT] commit

---
+++ src/main/java/org/thz/syncora/SyncoraApplication.java
@@ -0,0 +1,13 @@
+package org.thz.syncora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncoraApplication.class, args);
+    }
+
+}
